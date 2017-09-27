4a8357049f5c88362a8f1032cc6a194c9695a874
PensCup87 <bdt9123@gmail.com>
2017-09-27 13:08:47 -0400

    Started with If/else

--- weekOne_dayThree/Program.cs
+++ weekOne_dayThree/Program.cs
@@ -10,6 +10,37 @@ namespace weekOne_dayThree
     {
         static void Main(string[] args)
         {
+
+            //int firstNumber = 2;
+            //int secondNumber = 6;
+
+            //if ( firstNumber > secondNumber)
+            //{
+            //    //Do something
+            //    Console.WriteLine("Bobo taught you math.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hello Hello. Good job");
+            //}
+
+            Console.WriteLine("What day is today?");
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "Monday")
+            {
+                Console.WriteLine("Back to work");
+            }
+
+            else if (userAnswer == "Friday")
+            {
+                Console.WriteLine("TGIF");
+            }
+
+            else
+            {
+                Console.WriteLine("Just another day in paradise");
+            }
         }
     }
 }
